ded9e5903cdafd34ea091e6b1150380d190bae54
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-07-10 23:02:12 -0300

    config style:lint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
